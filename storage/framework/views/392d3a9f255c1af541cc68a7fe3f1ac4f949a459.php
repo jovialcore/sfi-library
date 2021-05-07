95959aa6a58ab858f41d4803e9c371e5d2f534d4
jovialcore <jollydosky@gmail.com>
2021-05-07 18:45:45 +0100

    okay seessin

--- storage/framework/views/392d3a9f255c1af541cc68a7fe3f1ac4f949a459.php
+++ storage/framework/views/392d3a9f255c1af541cc68a7fe3f1ac4f949a459.php
@@ -1,28 +1,32 @@
 <?php $__env->startSection('content'); ?>
     <div class="container-fluid" >
-      <div class="row header">
-      <div class="col-8 mx-auto search-wrapper">
-        <h4 class="text-white text-h text-center mb-4">SFI-IMAGE-LIBRARY: Search all media fiLes e.g photos for past programmes  </h4>
-        <form class=""  action="">
-          <div class="input-group mb-3">
-            <div class="input-group-prepend">
-              <button class="btn btn-light search-icon" type="button"><i class="fa fa-search"></i></button>
-            </div>
-            <input type="search" class="form-control" style="height:60px;" placeholder="Search images">
-            <div class="input-group-append">
-              <button type="button" class="btn btn-outline-secondary bg-white text-dark dropdown-toggle" data-toggle="dropdown">
-                Select image Category
-              </button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Convention 2021</a>
-                <a class="dropdown-item" href="#">YDF</a>
-                <a class="dropdown-item" href="#">Jesus Wives</a>
-              </div>
+
+<!-- navbar and stuff ends here -->
+<div class="container-fluid" >
+<div class="row header">
+    <div class="col-8 mx-auto search-wrapper">
+      <h4 class="text-white text-h text-center mb-4">SFI-IMAGE-LIBRARY: Search all media fiLes e.g photos for past programmes  </h4>
+      <form class=""  action="">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <button class="btn btn-light search-icon" type="button"><i class="fa fa-ssearchsearchearch"></i></button>
+          </div>
+          <input type="search" class="form-control" style="height:60px;" placeholder="Search images">
+          <div class="input-group-append">
+            <button type="button" class="btn btn-outline-secondary bg-white text-dark dropdown-toggle" data-toggle="dropdown">
+              Select image Category
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Convention 2021</a>
+              <a class="dropdown-item" href="#">YDF</a>
+              <a class="dropdown-item" href="#">Jesus Wives</a>
             </div>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
+  </div>
+    <!-- the section fo the images after a search -->
       <!-- the section fo the images after a search -->
 
 <div class="row mt-3">
@@ -34,7 +38,7 @@
     <?php $__currentLoopData = $allImages; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $image): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 
     <div class="grid-item">
-     <img src="<?php echo e(asset('storage/uplads/'.$image->name)); ?>" >
+     <img src="<?php echo e(asset('storage/uploads/'.$image->name)); ?>" >
     </div>
     
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
@@ -43,7 +47,10 @@
     </div>
   </div>
 </div>
+
 </div>
+
+    </div>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/jovialp/Desktop/searchLibrary sfI/SFI-LIBRARY/resources/views/home.blade.php ENDPATH**/ ?>
